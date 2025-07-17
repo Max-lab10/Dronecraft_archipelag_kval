41540aa27f3ec5b60f43bdc2f7cfe7f59333464f
Max-lab10 <solovyovev201182@gmail.com>
2025-07-17 14:58:46 +0700

    Update poletel.py

--- poletel.py
+++ poletel.py
@@ -1,11 +1,14 @@
+# Импорт необходимых библиотек
 import rospy
 from clover import srv
 from std_srvs.srv import Trigger
 import math
 from clover.srv import SetLEDEffect
 
+# Инициализация ноды 'flight'
 rospy.init_node('flight')
 
+# Создание объектов-прокси для работы с сервисами
 set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
@@ -16,6 +19,7 @@ set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
 set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
 land = rospy.ServiceProxy('land', Trigger)
 
+# Функция для ожидания прилёта в navigate-точку
 def wait_arrival(tolerance=0.2):
     while not rospy.is_shutdown():
         telem = get_telemetry(frame_id='navigate_target')
@@ -23,27 +27,28 @@ def wait_arrival(tolerance=0.2):
             break
         rospy.sleep(0.2)
 
-set_effect(effect='fill', r=0, g=0, b=255)
-navigate(z=1.5, auto_arm=True, frame_id='body')
+# Алгоритм полёта
+set_effect(effect='fill', r=0, g=0, b=255)  # Залить всю строку светодиодной ленты синим цветом
+navigate(z=1.5, auto_arm=True, frame_id='body')  # Взлёт на высоту 1.5 метра
 wait_arrival()
-navigate(y=-0.5, z=1.5, frame_id='aruco_24')
+navigate(y=-0.5, z=1.5, frame_id='aruco_24')  # Полёт в коородинаты x=0, y=-0.5, z=1.5 относительно аруко маркера 24
 wait_arrival()
-set_effect(effect='fade', r=0, g=255, b=0)
-rospy.sleep(3)
-navigate(y=0.5, z=1.5, frame_id='aruco_24')
+set_effect(effect='fade', r=0, g=255, b=0  # Плавное перетекание цветов светодиодной ленты в зелёный цвет
+rospy.sleep(3)  # Задержка 3 секунды
+navigate(y=0.5, z=1.5, frame_id='aruco_24')  #  Полёт в коородинаты x=0, y=0.5, z=1.5 относительно аруко маркера 24
 wait_arrival()
-set_effect(effect='wipe', r=255, g=255, b=0)
-rospy.sleep(3)
-navigate(y=0.5, z=2, frame_id='aruco_24')
+set_effect(effect='wipe', r=255, g=255, b=0)  # "надвигание" жёлтого цвета на светодиодной ленте
+rospy.sleep(3)  # Задержка 3 секунды
+navigate(y=0.5, z=2, frame_id='aruco_24')  # Полёт в коородинаты x=0, y=0.5, z=2 относительно аруко маркера 24
 wait_arrival()
-set_effect(effect='blink', r=0, g=255, b=255)
-rospy.sleep(3)
-navigate(y=-0.5, z=2, frame_id='aruco_24')
+set_effect(effect='blink', r=0, g=255, b=255)  # Мигание светодиодной ленты бирюзовым цветом
+rospy.sleep(3)  # Задержка 3 секунды
+navigate(y=-0.5, z=2, frame_id='aruco_24')  # Полёт в коородинаты x=0, y=-0.5, z=2 относительно аруко маркера 24
 wait_arrival()
-set_effect(effect='blink_fast', r=255, g=255, b=255)
+set_effect(effect='blink_fast', r=255, g=255, b=255)  # Быстрое мигание светодиодной ленты белым цветом
 wait_arrival()
-rospy.sleep(3)
-navigate(z=2, frame_id='aruco_42')
-set_effect(effect='rainbow')
+rospy.sleep(3)  #  Задержка 3 секунды
+navigate(z=2, frame_id='aruco_42')  # Полёт в координаты x=0, y=0, z=2 относительно аруко маркера 42
+set_effect(effect='rainbow')  # Переливание светодиодной ленты цветами радуги
 wait_arrival()
-land()
+land()  # Посадка
